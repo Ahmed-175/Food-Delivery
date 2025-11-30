5891eef9de741172405b539fbc6a3d02838e8408
Ahemd-175 <abmedfarag175@gmail.com>
2025-11-30 03:52:03 +0200

    complete cleaning data funciton

--- R/cleaning.R
+++ R/cleaning.R
@@ -1,30 +1,36 @@
-# R/cleaning.R
-
 clean_data <- function(df) {
   library(dplyr)
-  
-  df <- df %>%
-    # Remove rows with NA in important numeric columns
-    filter(!is.na(Distance_km), !is.na(Delivery_Time_min),
-           !is.na(Preparation_Time_min), !is.na(Courier_Experience_yrs)) %>%
-    
-    # Remove negative or extreme outliers
-    filter(Distance_km >= 0 & Distance_km <= 50,
-           Delivery_Time_min >= 5 & Delivery_Time_min <= 180,
-           Preparation_Time_min >= 0 & Preparation_Time_min <= 60,
-           Courier_Experience_yrs >= 0 & Courier_Experience_yrs <= 40) %>%
-    
-    # Correct data types
-    mutate(
-      Weather = factor(Weather),
-      Traffic_Level = factor(Traffic_Level),
-      Time_of_Day = factor(Time_of_Day),
-      Vehicle_Type = factor(Vehicle_Type),
 
-      # Feature engineering
-      Speed_kmph = Distance_km / (Delivery_Time_min / 60),
-      Late_Delivery = ifelse(Delivery_Time_min > 30, 1, 0)
-    )
+  df <- df[!duplicated(df), ]
+  df[df == ""] <- NA
+  df <- na.omit(df)
+
+
+  df$Order_ID <- as.integer(df$Order_ID)
+  df$Weather <- as.character(df$Weather)
+  df$Traffic_Level <- as.character(df$Traffic_Level)
+  df$Time_of_Day <- as.character(df$Time_of_Day)
+  df$Vehicle_Type <- as.character(df$Vehicle_Type)
+  df$Distance_km <- as.numeric(df$Distance_km)
+  df$Preparation_Time_min <- as.integer(df$Preparation_Time_min)
+  df$Courier_Experience_yrs <- as.numeric(df$Courier_Experience_yrs)
+  df$Delivery_Time_min <- as.integer(df$Delivery_Time_min)
+
+
+  out_dist <- boxplot(df$Distance_km, plot = FALSE)$out
+  out_prep <- boxplot(df$Preparation_Time_min, plot = FALSE)$out
+  out_cour <- boxplot(df$Courier_Experience_yrs, plot = FALSE)$out
+  out_del <- boxplot(df$Delivery_Time_min, plot = FALSE)$out
+
+
+  df <- df[-which(df$Distance_km %in% out_dist), ]
+  df <- df[-which(df$Preparation_Time_min %in% out_prep), ]
+  df <- df[-which(df$Courier_Experience_yrs %in% out_cour), ]
+  df <- df[-which(df$Delivery_Time_min %in% out_del), ]
+
+
+  df$Speed <- round(df$Distance_km / df$Delivery_Time_min, 2)
+  df$Late_Delivery <- ifelse(df$Delivery_Time_min > 30, 1, 0)
 
   return(df)
 }
